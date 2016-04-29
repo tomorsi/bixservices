8f3999a4447a7c9a45160193c6aebd65ebbd35d3
torsi <tom.orsi@yahoo.com>
2016-04-29 16:29:20 -0700

    modified:   PlayerPool/.classpath 	modified:   PlayerPool/src/com/zingaretti/bix/SkaterModel.java 	PlayerPool/WebContent/WEB-INF/lib/commons-dbcp2-2.1.1.jar

--- PlayerPool/src/com/zingaretti/bix/SkaterModel.java
+++ PlayerPool/src/com/zingaretti/bix/SkaterModel.java
@@ -1,7 +1,11 @@
 package com.zingaretti.bix;
 
 import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
+
+import org.apache.commons.dbcp2.BasicDataSource;
 
 public class SkaterModel implements Serializable {
 	
@@ -9,7 +13,38 @@ public class SkaterModel implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 5345148168978320712L;
+	
+	private BasicDataSource ds = null;
+	static private final String driverName = "org.postgresql.Driver";
+	static private final String connectionUrl ="jdbc:postgressql://bixdb/bixdb";
+	
+	SkaterModel()
+	{
+		ds = new BasicDataSource();
+		ds.setDriverClassName(driverName);
+		ds.setUsername("torsi");
+		ds.setPassword("borland6");
+		ds.setUrl(connectionUrl);
+	}
 
+	private Connection getConnection()
+	{
+		Connection c = null;
+		try {
+			c = ds.getConnection();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return c;
+	}
+	
+	public void addSkater(Skater s)
+	{
+	
+		
+	}
+	
 	public List<Skater> getFullList()
 	{
 		
