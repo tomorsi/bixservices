61a4692189de94891d92f68c7a0be58c83b8862b
torsi <tom.orsi@yahoo.com>
2016-04-29 17:59:49 -0700

    Changes for JAXB and Form PUT implementations

--- PlayerPool/src/com/zingaretti/bix/PlayerPoolService.java
+++ PlayerPool/src/com/zingaretti/bix/PlayerPoolService.java
@@ -3,18 +3,26 @@ package com.zingaretti.bix;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
+import javax.xml.bind.JAXBElement;
+
 
 @Path("/PlayerPoolService")
 public class PlayerPoolService {
 
    SkaterModel skaterModel = new SkaterModel();
    GoalieModel goalieModel = new GoalieModel(); 
+   
+   private static final String SUCCESS_RESULT = "<html>Success</html>";
+   private static final String FAILURE_RESULT = "<html>Failure</html>";
    
    @GET
    @Path("/skaters")
@@ -23,12 +31,36 @@ public class PlayerPoolService {
       return skaterModel.getFullList();
    }	
    
+   /*
+    * Create a skater object using the HTML url encoded form.
+    */
    @PUT
-   @Path("/skaters")
-   @Consumes(MediaType.APPLICATION_XML)
-   public void addSkater()
+   @Path("/skatersByForm")
+   @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+   @Produces(MediaType.TEXT_HTML)
+   public String addSkater(@FormParam("firstname") String firstname,
+		   @FormParam("lastname") String lastname,
+		   @FormParam("emailprimary") String emailprimary,
+		   @FormParam("smstextnumber") String smstextnumber,
+		   @FormParam("zipcode") String zipcode,
+		   @FormParam("password") String password)
    {
+	     
 	   
+	   return SUCCESS_RESULT;
+   }
+   
+   /*
+    * Create a skater object using the xml application type. 
+    */
+   @PUT
+   @Path("/skatersByXml")
+   @Consumes(MediaType.APPLICATION_XML)
+   @Produces(MediaType.TEXT_HTML)
+   public String addSkater(JAXBElement<Skater> skater)
+   {
+	     	   
+	   return SUCCESS_RESULT;
    }
    
    @POST
--- PlayerPool/src/com/zingaretti/bix/Skater.java
+++ PlayerPool/src/com/zingaretti/bix/Skater.java
@@ -1,5 +1,53 @@
 package com.zingaretti.bix;
 
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "skater")
 public class Skater {
+
+	
+	private String firstName;
+	private String lastName;
+	private String emailPrimary;
+	private String smsTextNumber;
+	private String zipCode;
+	private String password;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmailPrimary() {
+		return emailPrimary;
+	}
+	public void setEmailPrimary(String emailPrimary) {
+		this.emailPrimary = emailPrimary;
+	}
+	public String getSmsTextNumber() {
+		return smsTextNumber;
+	}
+	public void setSmsTextNumber(String smsTextNumber) {
+		this.smsTextNumber = smsTextNumber;
+	}
+	public String getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPwd(String password) {
+		this.password = password;
+	}
 
 }
